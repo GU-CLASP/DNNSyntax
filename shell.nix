c7d42cf1dbaef7bb3a180af6ac1eeed47fa9854e
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2017-06-07 13:22:14 +0200

    update env

--- shell.nix
+++ shell.nix
@@ -1,20 +1,23 @@
 
 { nixpkgs ? import <nixpkgs> {} }:
-let nixpkgs_source = nixpkgs.fetchFromGitHub {
-      #owner = "NixOS";
-      owner = "jyp";
-      repo = "nixpkgs";
-      rev = "b94e253e69046e519bcfb04d64bf3864bf912d19";
-      sha256 = "1wlja7hi6zn4i5ma3qmbbpn9dvh1q2c1qvvhdriaad8mhni3ikcj";
-    }; in
-with import nixpkgs_source {};
+let
+   nixpkgs_source = fetchTarball "https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.09pre108282.53835c93cb/nixexprs.tar.xz";
+   # nixpkgs_source = ~/repo/nixpkgs;
+   # nixpkgs_source = nixpkgs.fetchFromGitHub {
+   #    #owner = "NixOS";
+   #    owner = "jyp";
+   #    repo = "nixpkgs";
+   #    rev = "b94e253e69046e519bcfb04d64bf3864bf912d19";
+   #    sha256 = "1wlja7hi6zn4i5ma3qmbbpn9dvh1q2c1qvvhdriaad8mhni3ikcj";
+   #  };
+in with import nixpkgs_source {};
 
 (pkgs.python35.withPackages (ps: [
   (if stdenv.isDarwin then ps.tensorflowWithoutCuda else ps.tensorflowWithCuda)
   ps.numpy
   ps.Keras
-  ps.pandas
-  ps.statsmodels
+  # ps.pandas
+  # ps.statsmodels
   ps.scipy
   ps.h5py
   # ps.notebook # collision with python 2.7
